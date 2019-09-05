08a907090d6b03e4be7a84d5a5227ec823eb6528
Majli <mjandgabe@gmail.com>
2019-09-05 14:49:58 +0200

    new file:   sortingGame/.vs/sortingGame/v15/.suo 	new file:   sortingGame/.vs/sortingGame/v15/Server/sqlite3/db.lock 	new file:   sortingGame/.vs/sortingGame/v15/Server/sqlite3/storage.ide 	new file:   sortingGame/.vs/sortingGame/v15/Server/sqlite3/storage.ide-shm 	new file:   sortingGame/.vs/sortingGame/v15/Server/sqlite3/storage.ide-wal 	new file:   sortingGame/sortingGame.sln 	new file:   sortingGame/sortingGame/App.config 	new file:   sortingGame/sortingGame/Program.cs 	new file:   sortingGame/sortingGame/Properties/AssemblyInfo.cs 	new file:   sortingGame/sortingGame/bin/Debug/sortingGame.exe 	new file:   sortingGame/sortingGame/bin/Debug/sortingGame.exe.config 	new file:   sortingGame/sortingGame/bin/Debug/sortingGame.pdb 	new file:   sortingGame/sortingGame/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   sortingGame/sortingGame/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   sortingGame/sortingGame/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   sortingGame/sortingGame/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   sortingGame/sortingGame/obj/Debug/sortingGame.csproj.CoreCompileInputs.cache 	new file:   sortingGame/sortingGame/obj/Debug/sortingGame.csproj.FileListAbsolute.txt 	new file:   sortingGame/sortingGame/obj/Debug/sortingGame.csprojAssemblyReference.cache 	new file:   sortingGame/sortingGame/obj/Debug/sortingGame.exe 	new file:   sortingGame/sortingGame/obj/Debug/sortingGame.pdb 	new file:   sortingGame/sortingGame/sortingGame.csproj

---
+++ sortingGame/sortingGame/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random random = new Random();
+            int swapOne, swapTwo;
+
+            for (int i = 0; i < 6; i++)
+            {
+                list.Add(random.Next(0, 100));
+            }
+
+            Console.WriteLine("Swap the numbers in the list until they're in asceding numerical order.");
+
+            while (true)
+            {
+                swapOne = -1;
+                swapTwo = -1;
+
+                WriteOulist(list);
+
+                while (list.Contains(swapOne) == false)
+                {
+                    Console.WriteLine("what's the first number you want to swap?");
+                    swapOne = Convert.ToInt32(Console.ReadLine());
+                }
+
+                while (list.Contains(swapTwo) == false || swapTwo == swapOne)
+                {
+                    Console.WriteLine("what's the second number you want to swap?");
+                    swapTwo = Convert.ToInt32(Console.ReadLine());
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] == swapOne)
+                        list[i] = swapTwo;
+
+                    else if (list[i] == swapTwo)
+                        list[i] = swapOne;
+                }
+
+                List<int> sortedList = new List<int>(list);
+                sortedList.Sort();
+
+                if (list.SequenceEqual(sortedList) == true)
+                {
+                    WriteOulist(list);
+                    Console.WriteLine("You Win!");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void WriteOulist(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + ",");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
